87ab2b2fb066409a7813b05d56c97b0ef7afd188
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-02-21 16:21:48 +0530

    Add : Suggestions Form

---
+++ script.js
@@ -0,0 +1,22 @@
+let remainingMoney = 236000000000;
+
+function buyItem(itemContainer){
+    const cost = parseInt(itemContainer.getAttribute('data-cost'));
+    const quantity = parseInt(itemContainer.querySelector('input').value);
+    const totalCost = cost * quantity;
+
+    const audio = document.getElementById('myAudio');
+
+    if(remainingMoney >= totalCost){
+        remainingMoney -= totalCost;
+        updateRemainingMoney();
+        audio.play();
+        
+    } else {
+        alert("Not enough money to buy this quantity of items!");
+    }
+}
+
+function updateRemainingMoney(){
+    document.getElementById('remaining-money').innerText = `$${remainingMoney.toLocaleString()}`;
+}
