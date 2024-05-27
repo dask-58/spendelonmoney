92988b34b174b9bbb2d5c7c88af7ea5c2f407794
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-05-27 16:11:41 +0530

    Rebuild

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+'use strict';
+
 let remainingMoney = 236000000000;
+const audio = document.getElementById('myAudio');
 
-function buyItem(itemContainer){
-    const cost = parseInt(itemContainer.getAttribute('data-cost'));
-    const quantity = parseInt(itemContainer.querySelector('input').value);
-    const totalCost = cost * quantity;
+function buyItem(itemContainer) {
+  const cost = parseInt(itemContainer.getAttribute('data-cost'));
+  const quantity = parseInt(itemContainer.querySelector('input').value);
+  const totalCost = cost * quantity;
 
-    const audio = document.getElementById('myAudio');
-
-    if(remainingMoney >= totalCost){
-        remainingMoney -= totalCost;
-        updateRemainingMoney();
-        audio.play();
-
-    } else {
-        alert("Not enough money to buy this quantity of items!");
-    }
+  if (remainingMoney >= totalCost) {
+    remainingMoney -= totalCost;
+    updateRemainingMoney();
+    audio.play();
+  } else {
+    alert("Not enough money to buy this quantity of items!");
+  }
 }
 
-function updateRemainingMoney(){
-    document.getElementById('remaining-money').innerText = `$${remainingMoney.toLocaleString()}`;
+function updateRemainingMoney() {
+  document.getElementById('remaining-money').innerText = `$${remainingMoney.toLocaleString()}`;
 }
+
+const buyButtons = document.querySelectorAll('.item button');
+buyButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    buyItem(button.parentElement);
+  });
+});
